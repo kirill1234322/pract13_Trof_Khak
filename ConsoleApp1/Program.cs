2d0f81d63471b66a7f9428d0d607e5b9fc615e7e
kirill1234322 <kirilltrofimenko757@gmail.com>
2022-01-24 15:04:36 +0500

    start

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ввести А\nВвести B\nВыполнить операцию +\nВыполнить операцию -\nВыполнить операцию *\nВыполнить операцию /");
+            int a=0, b=0;
+            int vvod=int.Parse(Console.ReadLine());
+
+        }
+    }
+}
